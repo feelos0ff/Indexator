24bd0480ff7a709f075d631f94848e12141d778c
feelos0ff <kostia_k@bk.ru>
2014-06-07 17:37:05 +0400

    not tested

--- Debug/src/cpp/subdir.mk
+++ Debug/src/cpp/subdir.mk
@@ -47,7 +47,7 @@ CPP_DEPS += \
 src/cpp/%.o: ../src/cpp/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I/opt/PostgreSQL/9.3/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
