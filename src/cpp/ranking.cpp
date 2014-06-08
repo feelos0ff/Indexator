10b81c6d9d3f3573d4771aca53d1c25a89066a4c
feelos0ff <kostia_k@bk.ru>
2014-06-08 20:48:28 +0400

    Ranking added

--- src/cpp/ranking.cpp
+++ src/cpp/ranking.cpp
@@ -1,19 +1,77 @@
 /*
- * ranking.cpp
- *
- *  Created on: 02 июня 2014 г.
- *      Author: feelosoff
- */
+* ranking.cpp
+*
+* Created on: 02 июня 2014 г.
+* Author: feelosoff
+*/
 
 #include "../headers/ranking.h"
 
-PostingList* Ranking::CuttingList ( vector<string> &query, IndexTable *idx ) {
-    int len = query.size();
-    map<unsigned int, unsigned int> rank;
-    /*	for(int i = 0; i < len; ++i){
-    		PostingList *tmp = idx->FindPos(query[i]);
-    		double idf = _db.GetCountTxt() / (double)tmp->Length();
 
-    	}*/
-    return NULL;
+double Ranking::idf(unsigned long df, unsigned long N)
+{
+    if(df == 0)
+        return 0;
+    return log10((double)N/df);
+}
+/*
+double weight(unsigned long tf, unsigned long N, unsigned long df)
+{
+if(tf == 0)
+return 0;
+return (1 + log10(tf)) * idf(df, N);
+}
+*/
+double Ranking::bm25_weight(unsigned long tf, unsigned long df, unsigned long N, int lenD, int avgDl)
+{
+    double k = 2.0, b = 0.75;
+    return idf(df, N) * (tf * (k + 1) / (tf + k * (1 - b + b * (lenD) / avgDl ) ) );
+}
+
+
+PostingList* Ranking::CuttingList(vector<string> &query, IndexTable *idx)
+{
+    unsigned long len = query.size(); // количество слов в запросе
+    Statistics* stat = idx->GetStat(); // статистика
+    double AVGlen = stat->GetAverageLen(); // средняя длина док-та
+    unsigned long N = stat->GetCountTxt(); // кол-во документов
+    vector < pair < double, unsigned long > > rank(N);
+
+    for(unsigned long i = 0; i < len; i++)
+    {
+        PostingList* pl = idx->FindPos(query[i]); // загрузить постинг лист для слова из запроса
+        if(pl)
+        {
+            unsigned long df = pl->Length(); // получить количество вх. слова из запроса в док-ты
+            for(unsigned long docId = 0; docId < N; docId++)
+            {
+                unsigned long lenD = stat->GetTxtLen(docId); // длина текущего документа
+                unsigned long tf = pl->LengthEnt(docId); //получить кол-во вхождений в текущий документ
+                double W = bm25_weight(tf, df, N, lenD, AVGlen);	// вычислить вес слова
+                if(i == 0)
+                {
+                    pair<double, unsigned long> tempPair(W, docId);
+                    rank[docId] = tempPair;
+                }
+                else
+                {
+                    rank[docId].first += W; // добавить вес в нужный вектор
+                }
+                delete pl;
+            }
+        }
+    }
+
+    if(rank.empty())
+    {
+        cout << "По вашему запросу ничего не найдено. Попробуйте переформулироватьзапрос\n";
+        return NULL;
+    }
+    sort(rank.begin(), rank.end());
+
+    PostingList* result = new PostingList();
+    for(unsigned long i = _maxRank-1, k= 0; k < 200; k++,i--)
+       result->Add(rank[i].second, 0);
+
+   return result;
 }
--- src/headers/ranking.h
+++ src/headers/ranking.h
@@ -5,14 +5,24 @@
 #include "posting.h"
 #include "index.h"
 #include "dbworker.h"
+#include <algorithm>
 
-class Ranking {
-    unsigned long _maxRank;
-    DBWorker _db;
+class Ranking{
+	unsigned long _maxRank;
+	DBWorker _db;
 
 public:
-    Ranking ( unsigned long maxRank = 200 ) : _maxRank ( maxRank ) {}
-    PostingList* CuttingList ( vector<string>&, IndexTable* );
+
+	Ranking(unsigned long maxRank = 200):_maxRank(maxRank){}
+
+	PostingList* CuttingList(vector<string>&, IndexTable*);
+	PostingList* Bm25Ranking(); // ранжирование bm25
+	PostingList* tf_idfRanking();// обычное tf-idf ранжирование
+	PostingList* VectorRancking(); //векторное ранжирование
+	PostingList* ModelRancking(); // ранжирование с использованием языковых моделей
+
+    double bm25_weight(unsigned long tf, unsigned long df, unsigned long N, int lenD, int avgDl);
+    double idf(unsigned long df, unsigned long N);
 };
 
 #endif // RANKING_H
